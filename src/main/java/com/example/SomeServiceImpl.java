460469dcc4ddabe01ff140b254489622939bce12
Rob Maskell <robmaskell@gmail.com>
2017-05-07 23:33:21 +0100

    create project

---
+++ src/main/java/com/example/SomeServiceImpl.java
@@ -0,0 +1,9 @@
+package com.example;
+
+public class SomeServiceImpl implements SomeService {
+
+	public String getResponse() {
+		
+		return "Response from live service";
+	}
+}
---
+++ src/test/java/com/example/SomeServiceTestImpl.java
@@ -0,0 +1,10 @@
+package com.example;
+
+public class SomeServiceTestImpl implements SomeService {
+
+	public String getResponse() {
+		
+		return "Response from ***TEST*** service";
+	}
+	
+}
