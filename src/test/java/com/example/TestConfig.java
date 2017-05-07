c39856b30bc6a86c7167420c1a36cddaacf01fe7
Rob Maskell <robmaskell@gmail.com>
2017-05-08 00:11:47 +0100

    updates

--- src/test/java/com/example/TestConfig.java
+++ src/test/java/com/example/TestConfig.java
@@ -2,13 +2,11 @@ package com.example;
 
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Primary;
 
 @TestConfiguration
 public class TestConfig {
 
 	@Bean
-	@Primary
 	public SomeService getSomeTestService() {
 		return new SomeServiceTestImpl();
 	}
